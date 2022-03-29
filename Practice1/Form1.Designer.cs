be7e4b4ccea4f4c30d1237623eb64571c64c7a9b
Zac <Zac@RIPPLE-XRP>
2022-03-30 07:46:13 +1300

    Add project files.

---
+++ Practice1/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Practice1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.coffeeRequiredTextBox = new System.Windows.Forms.TextBox();
+            this.teaBagsRequiredTextBox = new System.Windows.Forms.TextBox();
+            this.costOfCoffeeTextBox = new System.Windows.Forms.TextBox();
+            this.costOfTeaBagsTextBox = new System.Windows.Forms.TextBox();
+            this.totalCostTextBox = new System.Windows.Forms.TextBox();
+            this.coffeeRequiredLabel = new System.Windows.Forms.Label();
+            this.teaBagsRequiredLabel = new System.Windows.Forms.Label();
+            this.costOfCoffeeLabel = new System.Windows.Forms.Label();
+            this.costOfTeaBagsLabel = new System.Windows.Forms.Label();
+            this.totalCostLabel = new System.Windows.Forms.Label();
+            this.coffeeDrinkersTextBox = new System.Windows.Forms.TextBox();
+            this.teaDrinkersTextBox = new System.Windows.Forms.TextBox();
+            this.coffeeDrinkersLabel = new System.Windows.Forms.Label();
+            this.teaDrinkersLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(87, 367);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(125, 45);
+            this.calculateButton.TabIndex = 0;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(244, 367);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(125, 45);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(399, 367);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(125, 45);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // coffeeRequiredTextBox
+            // 
+            this.coffeeRequiredTextBox.Location = new System.Drawing.Point(316, 123);
+            this.coffeeRequiredTextBox.Name = "coffeeRequiredTextBox";
+            this.coffeeRequiredTextBox.Size = new System.Drawing.Size(100, 22);
+            this.coffeeRequiredTextBox.TabIndex = 3;
+            // 
+            // teaBagsRequiredTextBox
+            // 
+            this.teaBagsRequiredTextBox.Location = new System.Drawing.Point(316, 168);
+            this.teaBagsRequiredTextBox.Name = "teaBagsRequiredTextBox";
+            this.teaBagsRequiredTextBox.Size = new System.Drawing.Size(100, 22);
+            this.teaBagsRequiredTextBox.TabIndex = 4;
+            // 
+            // costOfCoffeeTextBox
+            // 
+            this.costOfCoffeeTextBox.Location = new System.Drawing.Point(316, 214);
+            this.costOfCoffeeTextBox.Name = "costOfCoffeeTextBox";
+            this.costOfCoffeeTextBox.Size = new System.Drawing.Size(100, 22);
+            this.costOfCoffeeTextBox.TabIndex = 5;
+            // 
+            // costOfTeaBagsTextBox
+            // 
+            this.costOfTeaBagsTextBox.Location = new System.Drawing.Point(316, 259);
+            this.costOfTeaBagsTextBox.Name = "costOfTeaBagsTextBox";
+            this.costOfTeaBagsTextBox.Size = new System.Drawing.Size(100, 22);
+            this.costOfTeaBagsTextBox.TabIndex = 6;
+            // 
+            // totalCostTextBox
+            // 
+            this.totalCostTextBox.Location = new System.Drawing.Point(316, 301);
+            this.totalCostTextBox.Name = "totalCostTextBox";
+            this.totalCostTextBox.Size = new System.Drawing.Size(100, 22);
+            this.totalCostTextBox.TabIndex = 7;
+            // 
+            // coffeeRequiredLabel
+            // 
+            this.coffeeRequiredLabel.AutoSize = true;
+            this.coffeeRequiredLabel.Location = new System.Drawing.Point(151, 129);
+            this.coffeeRequiredLabel.Name = "coffeeRequiredLabel";
+            this.coffeeRequiredLabel.Size = new System.Drawing.Size(142, 16);
+            this.coffeeRequiredLabel.TabIndex = 8;
+            this.coffeeRequiredLabel.Text = "Coffee Required (Kgs):";
+            // 
+            // teaBagsRequiredLabel
+            // 
+            this.teaBagsRequiredLabel.AutoSize = true;
+            this.teaBagsRequiredLabel.Location = new System.Drawing.Point(164, 174);
+            this.teaBagsRequiredLabel.Name = "teaBagsRequiredLabel";
+            this.teaBagsRequiredLabel.Size = new System.Drawing.Size(129, 16);
+            this.teaBagsRequiredLabel.TabIndex = 9;
+            this.teaBagsRequiredLabel.Text = "Tea Bags Required:";
+            // 
+            // costOfCoffeeLabel
+            // 
+            this.costOfCoffeeLabel.AutoSize = true;
+            this.costOfCoffeeLabel.Location = new System.Drawing.Point(202, 220);
+            this.costOfCoffeeLabel.Name = "costOfCoffeeLabel";
+            this.costOfCoffeeLabel.Size = new System.Drawing.Size(91, 16);
+            this.costOfCoffeeLabel.TabIndex = 10;
+            this.costOfCoffeeLabel.Text = "Cost of coffee:";
+            // 
+            // costOfTeaBagsLabel
+            // 
+            this.costOfTeaBagsLabel.AutoSize = true;
+            this.costOfTeaBagsLabel.Location = new System.Drawing.Point(179, 265);
+            this.costOfTeaBagsLabel.Name = "costOfTeaBagsLabel";
+            this.costOfTeaBagsLabel.Size = new System.Drawing.Size(114, 16);
+            this.costOfTeaBagsLabel.TabIndex = 11;
+            this.costOfTeaBagsLabel.Text = "Cost of Tea Bags:";
+            // 
+            // totalCostLabel
+            // 
+            this.totalCostLabel.AutoSize = true;
+            this.totalCostLabel.Location = new System.Drawing.Point(222, 307);
+            this.totalCostLabel.Name = "totalCostLabel";
+            this.totalCostLabel.Size = new System.Drawing.Size(71, 16);
+            this.totalCostLabel.TabIndex = 12;
+            this.totalCostLabel.Text = "Total Cost:";
+            // 
+            // coffeeDrinkersTextBox
+            // 
+            this.coffeeDrinkersTextBox.Location = new System.Drawing.Point(205, 68);
+            this.coffeeDrinkersTextBox.Name = "coffeeDrinkersTextBox";
+            this.coffeeDrinkersTextBox.Size = new System.Drawing.Size(100, 22);
+            this.coffeeDrinkersTextBox.TabIndex = 13;
+            // 
+            // teaDrinkersTextBox
+            // 
+            this.teaDrinkersTextBox.Location = new System.Drawing.Point(435, 68);
+            this.teaDrinkersTextBox.Name = "teaDrinkersTextBox";
+            this.teaDrinkersTextBox.Size = new System.Drawing.Size(100, 22);
+            this.teaDrinkersTextBox.TabIndex = 14;
+            // 
+            // coffeeDrinkersLabel
+            // 
+            this.coffeeDrinkersLabel.AutoSize = true;
+            this.coffeeDrinkersLabel.Location = new System.Drawing.Point(84, 74);
+            this.coffeeDrinkersLabel.Name = "coffeeDrinkersLabel";
+            this.coffeeDrinkersLabel.Size = new System.Drawing.Size(102, 16);
+            this.coffeeDrinkersLabel.TabIndex = 15;
+            this.coffeeDrinkersLabel.Text = "Coffee Drinkers:";
+            // 
+            // teaDrinkersLabel
+            // 
+            this.teaDrinkersLabel.AutoSize = true;
+            this.teaDrinkersLabel.Location = new System.Drawing.Point(328, 74);
+            this.teaDrinkersLabel.Name = "teaDrinkersLabel";
+            this.teaDrinkersLabel.Size = new System.Drawing.Size(88, 16);
+            this.teaDrinkersLabel.TabIndex = 16;
+            this.teaDrinkersLabel.Text = "Tea Drinkers:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(622, 435);
+            this.Controls.Add(this.teaDrinkersLabel);
+            this.Controls.Add(this.coffeeDrinkersLabel);
+            this.Controls.Add(this.teaDrinkersTextBox);
+            this.Controls.Add(this.coffeeDrinkersTextBox);
+            this.Controls.Add(this.totalCostLabel);
+            this.Controls.Add(this.costOfTeaBagsLabel);
+            this.Controls.Add(this.costOfCoffeeLabel);
+            this.Controls.Add(this.teaBagsRequiredLabel);
+            this.Controls.Add(this.coffeeRequiredLabel);
+            this.Controls.Add(this.totalCostTextBox);
+            this.Controls.Add(this.costOfTeaBagsTextBox);
+            this.Controls.Add(this.costOfCoffeeTextBox);
+            this.Controls.Add(this.teaBagsRequiredTextBox);
+            this.Controls.Add(this.coffeeRequiredTextBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Name = "Form1";
+            this.Text = "103 Practice Test 1 2015";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox coffeeRequiredTextBox;
+        private System.Windows.Forms.TextBox teaBagsRequiredTextBox;
+        private System.Windows.Forms.TextBox costOfCoffeeTextBox;
+        private System.Windows.Forms.TextBox costOfTeaBagsTextBox;
+        private System.Windows.Forms.TextBox totalCostTextBox;
+        private System.Windows.Forms.Label coffeeRequiredLabel;
+        private System.Windows.Forms.Label teaBagsRequiredLabel;
+        private System.Windows.Forms.Label costOfCoffeeLabel;
+        private System.Windows.Forms.Label costOfTeaBagsLabel;
+        private System.Windows.Forms.Label totalCostLabel;
+        private System.Windows.Forms.TextBox coffeeDrinkersTextBox;
+        private System.Windows.Forms.TextBox teaDrinkersTextBox;
+        private System.Windows.Forms.Label coffeeDrinkersLabel;
+        private System.Windows.Forms.Label teaDrinkersLabel;
+    }
+}
+
---
+++ Practice1/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+GET the number of coffee drinkers
+GET the number of tea drinkers
+
+CALCULATE the amount of coffee required (in kg)
+( = number of coffee drinkers * amount of coffee
+ grounds for a cup * number of cups drunk per week)
+
+CALCULATE the number of teabags required
+( = number of tea drinkers * number of cups drunk per week)
+
+CALCULATE the cost of the coffee for the week
+( = amount of coffee required in kg * cost of 1kg coffee)
+
+CALCULATE the cost of the tea for the week
+( = number of teabags required / 100 * cost of box of tea)
+
+CALCULATE the total weekly cost of coffee and tea
+( = cost of coffee for week + cost of tea for week)
+   
+DISPLAY the amount of coffee required (in kg)
+DISPLAY the number of teabags required
+DISPLAY the cost of coffee required, formatted as currency
+DISPLAY the cost of teabags required, formatted as currency
+DISPLAY the cost of the weeks coffee and tea, as currency
+*/
+
+
+namespace Practice1
+{
+    public partial class Form1 : Form
+    {
+        //Name: Zac Pini
+        //Student ID: 1266745
+
+        //Assuming everyone drinks two cups per day for 5 days (i.e. morning and afternoon tea/coffee break)
+        const int CUPS_PER_WEEK = 10;
+        //Average amount of coffee grounds required to make a cup of coffee
+        //Assume this is also the amount of beans required, i.e. no wastage when grinding
+        const double COFFEE_GROUNDS_PER_CUP = 0.015;
+        //Cost of a 1kg bag of coffee beans is $25.00
+        const decimal COFFEE_COST = 25m;
+        //Cost of a box of 100 teabags is $4.50
+        const decimal TEA_COST = 4.50m;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //DECLARE LOCAL VARIABLES
+                int numberOfCoffeeDrinkers;
+                int numberofTeaDrinkers;
+
+                decimal coffeeRequired;
+                int teaBagsRequired;
+
+                decimal costOfCoffee;
+                decimal costOfTeaBags;
+                decimal totalCost;
+
+                //PARSE INPUT VALUES
+                 numberOfCoffeeDrinkers = int.Parse(coffeeDrinkersTextBox.Text);
+                 numberofTeaDrinkers = int.Parse(teaDrinkersTextBox.Text);
+
+                //NUMBER OF COFFEE DRINKERS
+                coffeeRequired = (decimal)numberOfCoffeeDrinkers * (decimal)COFFEE_GROUNDS_PER_CUP *(decimal)CUPS_PER_WEEK;
+                //NUMBER OF TEA DRINKERS
+                teaBagsRequired = numberofTeaDrinkers * CUPS_PER_WEEK;
+                //COFFEE REQUIRED
+                costOfCoffee = coffeeRequired * COFFEE_COST;
+                //TEA_COST REQUIRED
+                costOfTeaBags = (decimal)teaBagsRequired / 100 * TEA_COST;
+                //TOTAL COST
+                totalCost = costOfCoffee + costOfTeaBags;
+
+                //SET OUTPUT VALUES
+                coffeeRequiredTextBox.Text = coffeeRequired.ToString() + " kgs";
+                teaBagsRequiredTextBox.Text = teaBagsRequired.ToString();
+                costOfCoffeeTextBox.Text = costOfCoffee.ToString("c");
+                costOfTeaBagsTextBox.Text = costOfTeaBags.ToString("c");
+                totalCostTextBox.Text = totalCost.ToString("c");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            coffeeDrinkersTextBox.Clear();
+            teaDrinkersTextBox.Clear(); 
+            coffeeRequiredTextBox.Clear();
+            teaBagsRequiredTextBox.Clear();
+            costOfCoffeeTextBox.Clear();
+            costOfTeaBagsTextBox.Clear();
+            totalCostTextBox.Clear();   
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+                
+
+
+                
+
+
+
+
+                
